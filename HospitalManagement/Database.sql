4068490cc21953addb0f592235a4832712ff2d2f
Vijayan S <143008350+VijayanEnthusiastic@users.noreply.github.com>
2025-09-25 06:14:14 +0530

    Update Database.sql

---
+++ HospitalManagement/Database.sql
@@ -0,0 +1,25 @@
+create database hospitalmanagement;
+
+use hospitalmanagement;
+
+create table patients(
+	id int auto_increment PRIMARY KEY,
+    name varchar(100) NOT NULL,
+    age INT not null,
+    gender varchar(100) not null
+);
+
+create table doctors(
+		id INT auto_increment PRIMARY KEY,
+        name varchar(100) NOT NULL,
+        department varchar(100) NOT NULL
+); 
+
+create table appointments(
+		id int auto_increment PRIMARY KEY,
+        patient_id int NOT NULL,
+        doctor_id int NOT NULL,
+        appointment_date date NOT NULL,
+        foreign key (patient_id) REFERENCES patients(id),
+        foreign key (doctor_id) REFERENCES doctors(id)
+);
